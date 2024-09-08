1dce5f0687c788f3d666af9a8bbddc8beb5604a9
Your Name <you@example.com>
2024-09-07 23:37:08 -0400

    sidebar component implementaion

---
+++ components/Sidebar.tsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+
+const Sidebar = ({user}: SiderbarProps) => {
+  return (
+    <section className='sidebar'>
+        <nav className='flex flex-col gap-4'>
+            <Link href='/' className='mb-12 cursor-pointer items-center gap-2'>
+                <Image 
+                    src='/icons/logo.svg' 
+                    width={34}
+                    height={34}
+                    alt='Horizon logo'
+                    className='size-[24] max-xl:size-14'
+                />
+                <h1 className='sidebar-logo'>
+                    Horizon
+                </h1>
+            </Link>
+        </nav>
+    </section>
+  )
+}
+
+export default Sidebar
